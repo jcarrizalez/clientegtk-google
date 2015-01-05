8b5fcb28344ca0b84eb16265fc26ac3c996243f8
sitgem@gmail.com <sitgem@gmail.com@08779794-6722-af96-3d01-049b52adc2a9>
2015-01-05 19:39:29 +0000

    correpcion de error el che

--- web/cgi-bin/minimizar.py
+++ web/cgi-bin/minimizar.py
@@ -5,7 +5,7 @@ import cgi
 
 form = cgi.FieldStorage()
 if form.has_key('accion'):
-	#os.system('wmctrl -r "ARAUCA" -b toggle,shaded')
-	os.system('xdotool search --name "ARAUCA" windowminimize')
+	os.system('wmctrl -r "ARAUCA" -b toggle,shaded')
+	#os.system('xdotool search --name "ARAUCA" windowminimize')
 	#os.system("wmctrl -k on")
 	#wmctrl -r "ARAUCA" -b remove,fullscreen
