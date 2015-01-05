c6dc04f624a6a6be7568a4cf0541852a5f39f2f8
sitgem@gmail.com <sitgem@gmail.com@08779794-6722-af96-3d01-049b52adc2a9>
2015-01-05 19:20:04 +0000

    correpcion de error el checkout

--- web/cgi-bin/minimizar.py
+++ web/cgi-bin/minimizar.py
@@ -3,34 +3,9 @@
 import os
 import cgi
 
-print "Content-Type: text/html"
-print "<html>"
-print
-print '''<head>
-			<title>PuntoVenta - Minimizar</title>
-			<style>
-				button.btn_login {
-					font-size: 18px imortant!;
-					width: 32px;
-					height: 32px;
-					background: none;
-					border: none;
-					color: #ffffff;
-					cursor: pointer;
-				}
-			</style>
-		</head>
-		<body style='padding: 0; margin: 0; overflow: hidden; height: 100%;'>
-			<form method=POST enctype=multipart/form-data action=http://127.0.0.1:8888/cgi-bin/minimizar.py>
-				<input type='hidden' name='accion' value='minimizar'>
-				<button id='minimizar' class='btn_login'>-</button>
-			</form>
-		</body>
-	</html>'''
 form = cgi.FieldStorage()
 if form.has_key('accion'):
-	os.system('wmctrl -r "ARAUCA" -b toggle,shaded')
-
-	#os.system("evince /home/desarrollo/mozilla.pdf")
+	#os.system('wmctrl -r "ARAUCA" -b toggle,shaded')
+	os.system('xdotool search --name "ARAUCA" windowminimize')
 	#os.system("wmctrl -k on")
 	#wmctrl -r "ARAUCA" -b remove,fullscreen
