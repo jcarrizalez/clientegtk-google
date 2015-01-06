e83ec88be271f28289b0db9e6b29fcb2bf7ad1ac
sitgem@gmail.com <sitgem@gmail.com@08779794-6722-af96-3d01-049b52adc2a9>
2015-01-06 13:22:54 +0000

    pruebas en ejecucion

--- web/cgi-bin/minimizar.py
+++ web/cgi-bin/minimizar.py
@@ -3,9 +3,15 @@
 import os
 import cgi
 
+print "Content-Type: text/html"
+print "Access-Control-Allow-Origin: *"
+print "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"
+print "<html>"
+print
+
 form = cgi.FieldStorage()
 if form.has_key('accion'):
 	os.system('wmctrl -r ".ARAUCA." -b toggle,shaded')
-	#os.system('xdotool search --name ".ARAUCA." windowminimize')
+	os.system('xdotool search --name ".ARAUCA." windowminimize')
 	#os.system("wmctrl -k on")
 	#wmctrl -r ".ARAUCA." -b remove,fullscreen
