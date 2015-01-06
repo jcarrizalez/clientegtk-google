b8f60654b0c26a72555745acf37758e40d872e48
sitgem@gmail.com <sitgem@gmail.com@08779794-6722-af96-3d01-049b52adc2a9>
2015-01-06 13:22:54 +0000

    pruebas en ejecucion

--- trunk/web/cgi-bin/cerrar.py
+++ trunk/web/cgi-bin/cerrar.py
@@ -3,6 +3,12 @@
 import os
 import cgi
 
+print "Content-Type: text/html"
+print "Access-Control-Allow-Origin: *"
+print "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"
+print "<html>"
+print
+
 form = cgi.FieldStorage()
 if form.has_key('accion'):
 	os.system("/usr/share/PuntoVenta/cerrar")
