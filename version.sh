488eeabb14c8b1429dd0fcf9c4a5356e17726c5f
sitgem@gmail.com <sitgem@gmail.com@08779794-6722-af96-3d01-049b52adc2a9>
2015-01-19 17:29:34 +0000

    adecucion de capta huellas

--- version.sh
+++ version.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 SVN=`svn info /usr/share/PuntoVenta/ | grep 'Revisión:' | sed 's/Revisión://g'`;
-
 cp /usr/share/PuntoVenta/splash.png /tmp/splash.png;
-convert /tmp/splash.png -font helvetica -pointsize 15 -fill black -fill white -draw "text 8 25 $SVN" /tmp/splash_.png;
+convert /tmp/splash.png -font helvetica -pointsize 15 -fill black -fill white -draw  "text 8 25 'Revisión: $SVN'" /tmp/splash_.png;
 rm -f /tmp/splash.png;
-
 exit 0;
