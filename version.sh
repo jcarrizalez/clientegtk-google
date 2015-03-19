5701bbd324fa00e91cb05bc630b0fb42a9e1213b
Juan Manuel <juan@xperia>
2015-03-19 13:34:41 -0430

    Pruebas de Versionado con git

--- version.sh
+++ version.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-#SVN=`svn info /usr/share/PuntoVenta/ | grep 'Revisión:' | sed 's/Revisión://g'`;
-RUTarauca=`pwd`;cd /usr/share/PuntoVenta/;
-SVN=`git rev-list HEAD --count`;
-cd $RUTarauca;
+
+SVN=`svn info /usr/share/PuntoVenta/ | grep 'Revisión:' | sed 's/Revisión://g'`;
 cp /usr/share/PuntoVenta/splash.png /tmp/splash.png;
 convert /tmp/splash.png -font helvetica -pointsize 15 -fill black -fill white -draw  "text 8 25 'Revisión: $SVN'" /tmp/splash_.png;
 rm -f /tmp/splash.png;
