097f524187b6961f17cf0a8485a737592a075f90
Juan Manuel <juan@xperia>
2015-03-19 13:50:23 -0430

    Se corrigio error de GitHub al intentar subir archivos nuevamente

--- version.sh
+++ version.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-SVN=`svn info /usr/share/PuntoVenta/ | grep 'Revisión:' | sed 's/Revisión://g'`;
+#SVN=`svn info /usr/share/PuntoVenta/ | grep 'Revisión:' | sed 's/Revisión://g'`;
+RUTarauca=`pwd`;
+cd /usr/share/PuntoVenta/;
+SVN=`git rev-list HEAD --count`;
+cd $RUTarauca;
 cp /usr/share/PuntoVenta/splash.png /tmp/splash.png;
 convert /tmp/splash.png -font helvetica -pointsize 15 -fill black -fill white -draw  "text 8 25 'Revisión: $SVN'" /tmp/splash_.png;
 rm -f /tmp/splash.png;
